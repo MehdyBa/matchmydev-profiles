00f9f624a12e54a39dbee611c2a912db4ef0eba3
damla <subasi.paris@gmail.com>
2022-11-07 14:23:25 +0100

    change image file's name

--- front-vanilla/src/components/errors/unauthorized/unauthorized.js
+++ front-vanilla/src/components/errors/unauthorized/unauthorized.js
@@ -3,8 +3,8 @@ import { messages} from "../messages.js";
 
 function renderUnauthorized() {
     const content =
-   ` <h1>401 Unauthorized</h1>
-     <p>the request was not made because valid credentials for the targeted resource are missing.</p>
+   ` <h1>{messages.errors.unauthorizerd.h1}</h1>
+   <p>{messages.errors.unauthorizerd.p}</p>
      <img class="error-image" src="../../../assets/unauthorizedError.png" alt="401 unauthorized">
     `
     const main = document.getElementById("main");
