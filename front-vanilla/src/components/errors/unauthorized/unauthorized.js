c8cafc48dfafe363cd9fc5e8da95be1bc146b4d5
mehdy <mehdy.badaoui07@gmail.com>
2022-11-07 12:50:21 +0100

    first code add

--- front-vanilla/src/components/errors/unauthorized/unauthorized.js
+++ front-vanilla/src/components/errors/unauthorized/unauthorized.js
@@ -1,1 +1,14 @@
 /*unauthorized.js*/
+import { messages} from "../messages.js";
+
+function renderUnauthorized() {
+    const content =
+   ` <h1>401 Unauthorized</h1>
+     <p>the request was not made because valid credentials for the targeted resource are missing.</p>
+     <img class="error-image" src="../../../assets/unauthorizedError.png" alt="401 unauthorized">
+    `
+    const main = document.getElementById("main");
+    main.innerHTML = content;
+    
+};
+export {renderUnauthorized};
