530ecca25bc4045e92406b779382bce84a8ba467
damla <subasi.paris@gmail.com>
2022-11-07 12:47:21 +0100

    add img, change messages.js & messages.json & internalServerError

--- front-vanilla/src/components/errors/internalServerError/internalServerError.js
+++ front-vanilla/src/components/errors/internalServerError/internalServerError.js
@@ -1,1 +1,15 @@
-/*internalServerError.js*/
+import {messages} from "../messages.js";
+import internalServerError from internalServerError.css;
+
+function renderInternalServerError() {
+    const internalServerErrorContent = `
+    <h1>{messages.errors.internalServerError.h1}</h1>
+    <p>{messages.errors.internalServerError.p}</p>
+    <img  class="error-image" src = "../../../assets/internalServerError.svg">
+    
+    `
+    const main = document.getElementById("main");
+    main.innerHTML = internalServerErrorContent; 
+}
+
+export {renderInternalServerError};
