ab7b985612e28bfe32d5942edad41f9cb1f1d24a
Massinelo <massi.ait@gmail.com>
2022-11-07 12:51:09 +0100

    change on notFound component, init.js, router.js

--- front-vanilla/src/components/errors/notFound/notFound.js
+++ front-vanilla/src/components/errors/notFound/notFound.js
@@ -1,1 +1,15 @@
-/*notFound.js*/
+import "./notFound.css"
+
+function renderNotFound() {
+    const content = `
+    <h1>404 Page not found</h1>
+    <p>The ressource requested could not be found on this server !</p>
+    <img class="error-image"
+     src="../../../assets/notFound.svg"
+     alt="404 not found">`
+
+    const main = document.getElementById("main");
+    main.innerHTML = content;
+}
+
+export { renderNotFound };
--- front-vanilla/src/router.js
+++ front-vanilla/src/router.js
@@ -1,1 +1,25 @@
-/*router.js*/
+import { renderNotfound } from "./components/errors/notFound/notFound.js";
+import { renderForbidden } from "./components/errors/forbidden/forbidden.js"
+import { renderUnauthorized } from "./components/errors/unauthorized/unauthorized.js"
+import { renderInternalServerError } from "./components/errors/internalServerError/internalServerError.js"
+import { renderSearchProfiles } from "./components/searchProfiles/searchProfiles.js"
+
+window.addEventListener("hashchange", (event) => route(event));
+
+function route(event) {
+    const hash = window.location.hash;
+    if (hash == "/#/search-profiles" || hash == "") {
+        renderSearchProfiles();
+    } else if  (hash == "/#/not-found") {
+        renderNotfound();
+    } else if (hash == "/#/forbiden") {
+        renderForbidden();
+    } else if (hash == "/#/unauthorized") {
+        renderUnauthorized();
+    } else if (hash == "/#/internal-server-error") {
+        renderInternalServerError();
+    }
+
+}
+
+export { route };
