0a0f41ceab088eaa4e111b74dbcd83e28d4ebe8f
MilenaPC <milena771@hotmail.fr>
2022-11-07 12:50:44 +0100

    add forbidden,searchProfiles, and forbidden image

--- front-vanilla/src/components/errors/forbidden/forbidden.js
+++ front-vanilla/src/components/errors/forbidden/forbidden.js
@@ -1,1 +1,15 @@
-/*forbidden.js*/
+import {messages} from "../messages.js";
+
+function renderForbidden(){
+
+    const forbiddenContent = `
+        <h1>403 Forbidden</h1>
+        <p>You don't have the permission to access on this server</p>
+        <img class="error-image" src="../../../assets/forbidden.svg" alt="403 Forbidden">
+    `;
+
+    const main = document.getElementById("main");
+    main.innerHTML = forbiddenContent;
+}
+
+export {renderForbidden};
