88db030edf3889c394b01843acd4eb7858e5fc23
MilenaPC <milena771@hotmail.fr>
2022-11-18 17:35:01 +0100

    initial commit

---
+++ profiles-api/src/main/java/co/simplon/matchmydev/profiles/ProfilesApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.matchmydev.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesApiApplication.class, args);
+	}
+
+}
