bb69eb1d72017ccb59d32d12702552093159bb13
Massinelo <massi.ait@gmail.com>
2022-11-24 14:26:29 +0100

    some test

--- front-vanilla/src/components/searchProfiles/SearchProfilesComponent.js
+++ front-vanilla/src/components/searchProfiles/SearchProfilesComponent.js
@@ -10,7 +10,7 @@ export default class SearchProfilesComponent extends BaseIterableComponent {
             elementsSelector: "#profiles"
         });
     }
-    
+     
 
     template(){
         return `
