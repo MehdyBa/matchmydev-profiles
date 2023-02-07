049b3518a5e6f62329a01ea16db2561f3511b771
MilenaPC <milena771@hotmail.fr>
2023-02-07 15:39:58 +0100

    application properties to access profiles-api.cors.allowedOrigins

--- profiles-api/src/main/java/co/simplon/matchmydev/profiles/configuration/WebConfig.java
+++ profiles-api/src/main/java/co/simplon/matchmydev/profiles/configuration/WebConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-		registry.addMapping("/**").allowedOrigins("http://localhost:5500")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+	registry.addMapping("/**")
+		.allowedOrigins("${profiles-api.cors.allowedOrigins}")
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
 
-	}
+    }
 }
