121ba36aadceb83ff8a74248525077f4200fef8c
MilenaPC <milena771@hotmail.fr>
2023-02-08 15:51:26 +0100

    create table contract_types and insert data in new table contract_types

--- profiles-db/data.dml.sql
+++ profiles-db/data.dml.sql
@@ -1,7 +1,20 @@
+DELETE FROM contract_types;
 DELETE FROM profiles;
 
-INSERT INTO profiles(first_name, last_name, identifier, email, job_title, contract_type, hiring_date, avatar, description, tech_lead)
-	VALUES ('Damla', 'SUBASI', 'dsubasi030123', 'damla@subasi.com', 'Developer', 'Permanent', '2023-01-03', 'https://i.kym-cdn.com/entries/icons/original/000/021/441/chill_cat.jpg','Excellent full stack developer', false),
-	('Lisa', 'MUNSY', 'lmunsy030123', 'lisa@munsy.com', 'Developer', 'Permanent', '2023-01-03', 'https://encredebretagne.bzh/wp-content/uploads/2015/12/chat-breton.jpg','Curious duck lover', true),
-	('Laurine', 'DA SILVA', 'ldasilv030123', 'laurine@dasilva.com', 'Developer', 'Permanent', '2023-01-03', 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Nutkin.jpg', 'Best delegate ever', true);
+INSERT INTO contract_types(name)
+	VALUES('Permanent'),('Contractor');
+	
+INSERT INTO profiles(first_name, last_name, identifier, email, job_title, contract_id, hiring_date, avatar, description, tech_lead)
+	VALUES ('Damla', 'SUBASI', 'dsubasi030123', 'damla@subasi.com', 'Developer',(
+		SELECT id FROM contract_types WHERE name ='Permanent'
+		), '2023-01-03', 'https://i.kym-cdn.com/entries/icons/original/000/021/441/chill_cat.jpg','Excellent full stack developer', false),
+	('Lisa', 'MUNSY', 'lmunsy030123', 'lisa@munsy.com', 'Developer', (
+		SELECT id FROM contract_types WHERE name ='Contractor'
+		), '2023-01-03', 'https://encredebretagne.bzh/wp-content/uploads/2015/12/chat-breton.jpg','Curious duck lover', true),
+	('Laurine', 'DA SILVA', 'ldasilv030123', 'laurine@dasilva.com', 'Developer', (
+		SELECT id FROM contract_types WHERE name ='Permanent'
+		), '2023-01-03', 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Nutkin.jpg', 'Best delegate ever', true),
+	('Milena', 'MANNEVILLE', 'mmannev030123', 'milena@mannev.com', 'Developer', (
+		SELECT id FROM contract_types WHERE name ='Contractor'
+		), '2023-01-03', 'https://www.lolchat.fr/wp-content/uploads/2014/05/chat-lunettes-soleil.jpg', 'Always learning', false);
 	
--- profiles-db/schema.ddl.sql
+++ profiles-db/schema.ddl.sql
@@ -1,4 +1,10 @@
+DROP TABLE IF EXISTS contract_types;
 DROP TABLE IF EXISTS profiles;
+
+CREATE TABLE contract_types(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(200) UNIQUE NOT NULL
+);
 
 CREATE TABLE profiles (
 	id SERIAL PRIMARY KEY,
@@ -7,7 +13,10 @@ CREATE TABLE profiles (
 	identifier VARCHAR(13) NOT NULL,
 	email VARCHAR(155) UNIQUE NOT NULL,
 	job_title VARCHAR(50) NOT NULL,
-	contract_type VARCHAR(50) NOT NULL,
+	contract_id INTEGER,
+	CONSTRAINT fk_contract_id
+		FOREIGN KEY (contract_id)
+		REFERENCES contract_types(id),
 	hiring_date TIMESTAMP NOT NULL,
 	avatar VARCHAR(300) NOT NULL,
 	description VARCHAR(1000) NOT NULL,
