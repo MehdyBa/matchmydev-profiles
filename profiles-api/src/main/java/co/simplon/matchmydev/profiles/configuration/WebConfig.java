de2d6062cecd49ec7ef76f232faf2b7d9f1c74b9
MilenaPC <milena771@hotmail.fr>
2023-02-08 10:32:39 +0100

    access application.properties from WebConfig

--- profiles-api/src/main/java/co/simplon/matchmydev/profiles/configuration/WebConfig.java
+++ profiles-api/src/main/java/co/simplon/matchmydev/profiles/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package co.simplon.matchmydev.profiles.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${profiles-api.cors.allowedOrigins}")
+    private String myAllowedApi;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-	registry.addMapping("/**")
-		.allowedOrigins("${profiles-api.cors.allowedOrigins}")
+	registry.addMapping("/**").allowedOrigins(myAllowedApi)
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
 
     }
