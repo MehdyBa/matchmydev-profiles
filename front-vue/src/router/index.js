f7bad6de0dd61016b94138a49591042da36a057f
DamlaSub <mme.dalloway@gmail.com>
2023-02-09 15:03:07 +0100

    add edit profile component, edit router

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SearchProfilesView from '../views/SearchProfilesView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SearchProfilesView from "../views/SearchProfilesView.vue";
+import EditProfile from "../components/EditProfile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/profiles',
-      name: 'search-profiles',
-      component: SearchProfilesView
-    }
-  ]
-})
+      path: "/profiles",
+      name: "search-profiles",
+      component: SearchProfilesView,
+    },
+    {
+      path: "/profiles/1",
+      name: "edit-profile",
+      component: EditProfile,
+    },
+  ],
+});
 
-export default router
+export default router;
