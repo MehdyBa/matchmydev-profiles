1a4bf5e6174d7d1a20718867a9d5b34546257fec
Massinelo <massi.ait@gmail.com>
2023-03-09 11:10:08 +0100

    refactor backend

--- profiles-api/src/main/java/co/simplon/matchmydev/profiles/controllers/ProfileController.java
+++ profiles-api/src/main/java/co/simplon/matchmydev/profiles/controllers/ProfileController.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,15 +30,14 @@ public class ProfileController {
 	return service.getAll();
     }
 
-    @GetMapping("/{id}")
-    public ProfileDetailView getProfile(@PathVariable("id") Long id) {
-	return service.getProfile(id);
+    @GetMapping("/my")
+    public ProfileDetailView getProfile() {
+	return service.getProfile(1L);
     }
 
-    @PatchMapping("/{id}")
-    public void update(@ModelAttribute ProfileUpdateDto inputs,
-	    @PathVariable("id") Long id) {
-	service.update(inputs, id);
+    @PatchMapping("/my")
+    public void update(@ModelAttribute ProfileUpdateDto inputs) {
+	service.update(inputs, 1L);
 
 	System.out.println(inputs.getAvatar().getOriginalFilename());
 
