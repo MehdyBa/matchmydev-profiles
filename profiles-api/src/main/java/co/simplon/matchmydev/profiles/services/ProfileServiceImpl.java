6f1b5a822d98c9865e643bfc325136f692d6130e
Massinelo <massi.ait@gmail.com>
2023-03-10 19:05:25 +0100

    back upload function

--- profiles-api/src/main/java/co/simplon/matchmydev/profiles/services/ProfileServiceImpl.java
+++ profiles-api/src/main/java/co/simplon/matchmydev/profiles/services/ProfileServiceImpl.java
@@ -1,8 +1,17 @@
 package co.simplon.matchmydev.profiles.services;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Collection;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import co.simplon.matchmydev.profiles.dtos.ProfileDetailView;
 import co.simplon.matchmydev.profiles.dtos.ProfileUpdateDto;
@@ -12,29 +21,45 @@ import co.simplon.matchmydev.profiles.repositories.ProfileRepository;
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
-	private ProfileRepository profiles;
+    private ProfileRepository profiles;
 
-	public ProfileServiceImpl(ProfileRepository profiles) {
-		this.profiles = profiles;
+    @Value("${profiles-api.uploads.location}")
+    private String uploadDir;
+
+    public ProfileServiceImpl(ProfileRepository profiles) {
+	this.profiles = profiles;
+    }
+
+    @Override
+    public Collection<ProfileView> getAll() {
+	return profiles.findAllProjectedBy();
+    }
+
+    @Override
+    public ProfileDetailView getProfile(Long id) {
+	return profiles.findProjectedById(id);
+    }
+
+    @Override
+    public void update(ProfileUpdateDto inputs, Long id) {
+	Profile entity = profiles.findById(id).get();
+	entity.setDescription(inputs.getDescription());
+	MultipartFile file = inputs.getAvatar();
+	String baseName = UUID.randomUUID().toString();
+	String fileName = baseName + ".gif";
+	entity.setAvatar(fileName);
+	profiles.save(entity);
+	store(file, fileName);
+
+    }
+
+    private void store(MultipartFile file, String fileName) {
+	Path uploadPath = Paths.get(uploadDir);
+	Path target = uploadPath.resolve(fileName);
+	try (InputStream in = file.getInputStream()) {
+	    Files.copy(in, target, StandardCopyOption.REPLACE_EXISTING);
+	} catch (IOException ex) {
+	    throw new RuntimeException(ex);
 	}
-
-	@Override
-	public Collection<ProfileView> getAll() {
-		return profiles.findAllProjectedBy();
-	}
-
-	@Override
-	public ProfileDetailView getProfile(Long id) {
-		return profiles.findProjectedById(id);
-	}
-
-	@Override
-	public void update(ProfileUpdateDto inputs, Long id) {
-		Profile entity = profiles.findById(id).get();
-		entity.setDescription(inputs.getDescription());
-
-		profiles.save(entity);
-
-	}
-
+    }
 }
